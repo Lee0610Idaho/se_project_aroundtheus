f525b4b96993cafcd57d0e5f96d6ee3c334cbede
Jackie Lee <jackieleeid2000@gmail.com>
2024-07-20 11:36:41 -0600

    changed cards variable to a const and reconnected js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initalCards = [
+const initalCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
